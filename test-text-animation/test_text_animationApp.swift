c6ec7dbd9ba15c54624ba82461569e2cc09276fd
Tomáš Kafka <tk@tomaskafka.com>
2022-12-21 13:12:06 +0100

    Initial Commit

---
+++ test-text-animation/test_text_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_text_animationApp.swift
+//  test-text-animation
+//
+//  Created by Tomas Kafka on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_text_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
