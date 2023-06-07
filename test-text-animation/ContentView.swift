22236c6d5ff6d9022d86da7d58a9624a90e2a3aa
Tomáš Kafka <tk@tomaskafka.com>
2023-06-07 10:44:58 +0200

    Solve text animation through .contentTransition(.identity), but symbol still has a same problem

--- test-text-animation/ContentView.swift
+++ test-text-animation/ContentView.swift
@@ -5,12 +5,15 @@
 //  Created by Tomas Kafka on 21.12.2022.
 //
 
+import Foundation
 import SwiftUI
+
+// MARK: - Debug
 
 public struct DebugFrame: ViewModifier {
 	var color: UIColor
 	var lineWidth: CGFloat = 1
-	
+
 	public func body(content: Content) -> some View {
 		content
 			.overlay(
@@ -27,62 +30,131 @@ public extension View {
 	}
 }
 
+// MARK: - Backport
+
+public struct Backport<Content> {
+	let content: Content
+}
+
+public extension View {
+	var backport: Backport<Self> { Backport(content: self) }
+}
+
+public extension Backport where Content: View {
+	@ViewBuilder func contentTransitionNumericText() -> some View {
+		if #available(iOS 16.0, watchOS 9.0, tvOS 16.0, macCatalyst 16.0, macOS 13.0, *) {
+			content
+				.contentTransition(.numericText())
+		} else {
+			content
+		}
+	}
+
+	@ViewBuilder func contentTransitionIdentity() -> some View {
+		if #available(iOS 16.0, watchOS 9.0, tvOS 16.0, macCatalyst 16.0, macOS 13.0, *) {
+			content
+				.contentTransition(.identity)
+		} else {
+			content
+		}
+	}
+}
+
+
+// MARK: - Random symbols
+
+let symbols: [String] = [
+	"ellipsis.circle.fill",
+	"heart.fill",
+	"cloud.fill",
+	"checkmark.circle.fill",
+	"bolt.fill"
+]
+
+func randomItem<T>(from array: [T]) -> T? {
+	guard !array.isEmpty else { return nil }
+	let randomIndex = Int.random(in: 0 ..< array.count)
+	return array[randomIndex]
+}
+
+
+// MARK: - Views
+
 extension CGPoint {
 	func rounded(_ rounding: CGFloat) -> CGPoint {
 		return .init(
-			x: (self.x / rounding).rounded() * rounding,
-			y: (self.y / rounding).rounded() * rounding
+			x: (x / rounding).rounded() * rounding,
+			y: (y / rounding).rounded() * rounding
 		)
 	}
 }
 
+struct SymbolView: View {
+	var systemName: String
+	var size: CGFloat
+
+	var body: some View {
+		ZStack(alignment: .center) {
+			Image(systemName: systemName)
+			// .resizable()
+				.id("symbol")
+				.backport.contentTransitionIdentity()
+		}
+		.frame(width: size, height: size)
+		.id("zstack")
+		.backport.contentTransitionIdentity()
+	}
+}
+
+
 struct FollowFingerText: View {
 	@State private var currentPosition: CGPoint = .zero
-	
+
 	let circleSize: CGFloat = 44
 	let rounding: CGFloat = 20
-	
+
 	let version = UIDevice.current.systemVersion
 	var hint: String {
 		if version.starts(with: "15") {
-			return "iOS \(version): changing text animates its position smoothly"
+			return "iOS \(version): changing symbol animates its position smoothly"
 		} else {
-			return "iOS \(version): once the text changes, it is recreated at new origin for every animation step"
+			return "iOS \(version): once the symbol changes, it is recreated at new origin for every animation step"
 		}
 	}
-	
+
 	var body: some View {
-		GeometryReader { proxy in
+		GeometryReader { _ in
 			VStack(alignment: .leading) {
 				Text("Static text.")
 					.font(.headline)
-					.offset(x: currentPosition.x, y: currentPosition.y)
 					.id("text1")
 					.animation(.default, value: currentPosition)
-				
+
 				Text("Dynamic: \(Int(currentPosition.x)),\(Int(currentPosition.y))")
 					.font(.headline)
 					// .animation(nil, value: 1) /// doesn't help
-					.offset(x: currentPosition.x, y: currentPosition.y)
 					.id("text2")
 					.animation(.default, value: currentPosition)
 
 				Text(hint)
 					.font(.footnote)
-					.offset(x: currentPosition.x, y: currentPosition.y)
 					.id("hint")
+
+				if let randomSymbol = randomItem(from: symbols) {
+					SymbolView(systemName: randomSymbol, size: 32)
+						.id("symbol")
+				}
 
 				Circle()
 					.fill(.red)
 					.frame(width: circleSize, height: circleSize)
-					.offset(x: currentPosition.x, y: currentPosition.y)
 					.id("circle")
 					.animation(.default, value: currentPosition)
-
 			}
 			.frame(maxWidth: 180, alignment: .topLeading)
-			// .debugFrame()
-			
+			.offset(x: currentPosition.x, y: currentPosition.y)
+			.backport.contentTransitionIdentity()
+			// .contentTransition(.identity)
 		} /// geo reader
 		.contentShape(Rectangle())
 		.gesture(
